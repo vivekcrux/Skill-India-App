2c70384e301435517556b006023105a333ffc1eb
Nihit Bansal <nihitb06.dev@gmail.com>
2017-12-17 20:30:44 +0530

    Removed Koral Dependency

--- Android App/app/src/main/java/co/ardulous/skillindia/MainActivity.java
+++ Android App/app/src/main/java/co/ardulous/skillindia/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
     //This actually serves a purpose of MainActivity
@@ -11,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_splashscreen);
+
+        Glide.with(this).load(R.drawable.preloader).into((ImageView) findViewById(R.id.preLoader));
 
         new Handler().postDelayed(new Runnable() {
             @Override
