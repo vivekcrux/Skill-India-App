76df1a1826317143278707b6d3fce705890e425c
Nihit Bansal <nihitb06.dev@gmail.com>
2017-12-17 23:13:51 +0530

    Added missing files

---
+++ Android App/app/src/main/java/co/ardulous/skillindia/UpdatesFragment.java
@@ -0,0 +1,24 @@
+package co.ardulous.skillindia;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class UpdatesFragment extends Fragment {
+
+
+    public UpdatesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_updates, container, false);
+    }
+
+}
